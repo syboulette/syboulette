9fe418c7e0124f22c6f8a80dd8f0db04e9280f4b
Sybille <115070033+syboulette@users.noreply.github.com>
2024-11-08 15:41:15 +0100

    finish exercise

--- fullstack-challenges-master-1/01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ fullstack-challenges-master-1/01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,7 +1,22 @@
+STOP_MESSAGE = "I am going to work right now!"
+
 def coach_answer(your_message)
-  # TODO: return coach answer to your_message
+  if your_message == STOP_MESSAGE || your_message == STOP_MESSAGE.upcase
+    ""
+  elsif your_message.end_with?("?")
+    "Silly question, get dressed and go to work!"
+  else
+    "I don't care, get dressed and go to work!"
+  end
 end
 
 def coach_answer_enhanced(your_message)
-  # TODO: return coach answer to your_message, with additional custom rules of yours!
+  answer = coach_answer(your_message)
+  if answer == ""
+    ""
+  elsif your_message.upcase == your_message
+    "I can feel your motivation! #{answer}"
+  else
+    answer
+  end
 end
--- fullstack-challenges-master-1/01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ fullstack-challenges-master-1/01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,10 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
+coach_answer = nil
+
+while coach_answer != ""
+  puts "what do you want?"
+  your_message = gets.chomp.to_s
+  coach_answer = coach_answer_enhanced(your_message)
+  puts coach_answer
+end
