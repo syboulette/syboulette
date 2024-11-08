d6f7b14eb41cddc21c359fe7bc9e9b0b7019dff1
Sybille <115070033+syboulette@users.noreply.github.com>
2024-11-08 10:55:09 +0100

    finished ruby programming basics whats your name

--- fullstack-challenges-master-1/01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
+++ fullstack-challenges-master-1/01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
@@ -5,3 +5,16 @@ require_relative "compute_name"
 # TODO: ask for the last name
 # TODO: Print a greeting to the user with the full name concatenated,
 #       something like "Hello, Boris Paillard"
+puts "What's your first name?"
+first_name = gets.chomp.to_s
+
+puts "What's your middle name?"
+middle_name = gets.chomp.to_s
+
+puts "What's your last name?"
+last_name = gets.chomp.to_s
+
+your_name = compute_name(first_name, middle_name, last_name)
+
+# Finally, print user's age in days:
+puts "Welcome #{your_name} !"
